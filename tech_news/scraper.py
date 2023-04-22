5ca848129a49d5293f67fc3d10674272c1b78b72
Bianca Oura <biancaoura@gmail.com>
2023-04-21 22:09:30 -0300

    feat: scrape news func

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,9 +25,27 @@ def scrape_next_page_link(html_content):
     return selector.css(".next ::attr(href)").get()
 
 
-# Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".meta-author span.author a::text").get()
+    reading_time = selector.css(".meta-reading-time::text").get().split()[0]
+    summary = "".join(
+        selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+    ).strip()
+    category = selector.css(".category-style .label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time),
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
